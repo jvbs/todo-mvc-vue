fcfd709b63aefae578d7b68472a4efdeabae4ced
Joao Vitor Barbosa Souza <jvbs98@hotmail.com>
2020-06-08 15:11:56 -0300

    initial commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="todo">
+    <input type="text" class="todo-input" placeholder="What needs to be done?">
+    <span>TodoList</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+</style>
